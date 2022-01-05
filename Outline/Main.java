f625d926f6c40a11706c74f25ccd422f8bd9c78a
codytheking <codytheking93@gmail.com>
2022-01-05 09:56:35 -0800

    Code outline

---
+++ Outline/Main.java
@@ -0,0 +1,33 @@
+/**
+ * Highest GC-Content of given strands.
+ *
+ * @author Mr. King
+ * @version 01/04/2022
+ */
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        // Ask how many strands that will be input
+        // Create two String arrays, one for the ids and one for the strands
+        
+        // Get input for each id and strand 
+        //      Use a loop to ask for the specified number of id and strands
+        //      Ask for each id and strand separately
+        //      Insert each into their respective arrays
+
+        // Use method to get index in array of strand with greatest GC-Content
+        // Print its id and GC-Content as a percentage
+    }
+
+
+    // Create method that returns the GC-Content of a given strand
+    
+    
+
+    // Create a method that finds the strand with the greatest GC-Content 
+    // and returns its index in the arrays
+
+}
+
